7a5e522e03c0497bec3a28252fe357720b9bcae7
Naísa Melo <naisamelo@gmail.com>
2022-09-08 19:49:35 -0300

    Trabalhando com String

---
+++ Script - python/teste.py
@@ -0,0 +1,4 @@
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+sexo = input("Digite seu sexo: ")
+print("Seu nome é: {} tem {} anos do sexo {}.".format(nome, idade,sexo))
