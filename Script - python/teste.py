95cd6282e385f2ad69eb8a0126808106a16b22ac
Naísa Melo <naisamelo@gmail.com>
2022-09-12 20:53:35 -0300

    aula 10

--- Script - python/teste.py
+++ Script - python/teste.py
@@ -1,4 +1,4 @@
 nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 sexo = input("Digite seu sexo: ")
-print("Seu nome é: {} tem {} anos do sexo {}.".format(nome, idade,sexo))
+print("Seu nome é {} tem {} anos do sexo {}.".format(nome, idade,sexo))
