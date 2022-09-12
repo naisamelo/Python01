2a8ca489204100b14f7e8ff806b9aea6d80cca87
Naísa Melo <naisamelo@gmail.com>
2022-09-12 20:57:56 -0300

    Update ex_28_jogo_adivinhacao.py

--- Script - python/Exercicios/ex_28_jogo_adivinhacao.py
+++ Script - python/Exercicios/ex_28_jogo_adivinhacao.py
@@ -1,12 +1,12 @@
 from random import randint
+from time import sleep
 computador = randint(0, 5) #Faz o computador "Pensar"
-
 print('-=-' * 20)
 print('vou pensar em um número entre 0 e 5. Tente adivinhar...')
 print('-=-' * 20)
-
 jogador = int(input("Em que número eu pensei? ")) #Jogador tenta adivinhar
-
+print('PROCESSAND0....')
+sleep(3)
 if computador == jogador:
     print("Parabéns, você consegiu me vencer!")
 else:
