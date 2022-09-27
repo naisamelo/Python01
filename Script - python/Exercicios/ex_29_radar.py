04123f667119e50542a9d8d63d95643dcc4b9987
Naísa Melo <naisamelo@gmail.com>
2022-09-27 20:18:05 -0300

    Update ex_29_radar.py
    espaços aplicados entre os operadores

--- Script - python/Exercicios/ex_29_radar.py
+++ Script - python/Exercicios/ex_29_radar.py
@@ -1,6 +1,6 @@
 vel = float(input('Digite a velocidade do carro: '))
 if(vel > 80):
     print('MULTADO! Você excedeu o limite permitido de 80km/h')
-    multa = (vel - 80)*7
+    multa = (vel - 80) * 7
     print('Você deve pagar uma multa de R${:.2f}!'.format(multa))
 print('Tenha um bom dia! Dirija com segurança!')
